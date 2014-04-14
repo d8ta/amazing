c4c3d1052563d46b75f62b8d893e8a36c8f7fa24
Daniel Raudschus <draudschus.mmt-b2013@fh.salzburg.ac.at>
2014-04-14 08:20:46 +0200

    implement moving with arrowkeys

--- main.js
+++ main.js
@@ -3,26 +3,35 @@
  */
 $(document).ready(function () {
 
+    /**
+     * basic variables for X, Y, Width and Height
+     * @type {number}
+     */
+    var playerX = 10;
+    var playerY = 75;
+    var playerW = 5;
+    var playerH = 5;
+
+    var canvasX = 0;
+    var canvasY = 0;
+    var canvasWidth = 800;
+    var canvasHeight = 400;
+
+
     // Get the Canvas Elemten by css id
     var canvas = document.getElementById("canvas");
 
-    function drawCanvas(canvasX, canvasY, canvasWidth, canvasHeight) {
+    function drawCanvas() {
         var back = canvas.getContext('2d');
         back.fillStyle = 'grey';
         back.fillRect(canvasX, canvasY, canvasWidth, canvasHeight);
     }
 
-
-    /**
-     * Setting the Player with x and y
-     * @param posX
-     * @param posY
-     */
-    function drawPlayer(posX, posY, width, height) {
+    function drawPlayer() {
         var player = canvas.getContext("2d");
         player.fillStyle = 'white';
         // x, y, width, heigth
-        player.fillRect(posX, posY, width, height);
+        player.fillRect(playerX, playerY, playerW, playerH);
     }
 
     /**
@@ -43,59 +52,40 @@ $(document).ready(function () {
         line.closePath();
         line.stroke();
     }
+
     // ToDo: Function for Random Linemaking ie Maze
     /**
      * HINT: To do that just use random toX, toY or add random and defined
      * numbers to toX, toY.
      */
 
-    var posX = 0, posY = 0, width = 5, height = 5;
-    setInterval(function(){
-        drawCanvas(0, 0, 800, 400);
-        drawPlayer(posX, posY, width, height);
-        posX ++;
-        if  (posX > 100) {
-            posY++;
-            posX --;
-            if (posY > 100) {
-                posY --;
-                posX ++;
+    setInterval(function () {
+
+        /** Player is set **/
+        drawCanvas();
+        drawPlayer();
+
+        function movePlayer(key) {
+            // left
+            if (key.keyCode == 39) {
+                playerX += 5;
+            }
+            // right
+            if (key.keyCode == 37) {
+                playerX -= 5;
+            }
+            // down
+            if (key.keyCode == 40) {
+                playerY += 5;
+            }
+            // up
+            if (key.keyCode == 38) {
+                playerY -= 5;
             }
         }
 
+        document.onkeydown = movePlayer;
+
     }, 30);
-
-    /**
-     * Player movement via keys:
-     *      setInterval and let paint the canvas with the background
-     *      (every 30 milsec.) use switch statement for movement
-     */
-
-
-    /*function movePlayer(key) {
-        var moveX, moveY;
-        movePlayer = movePlayer || window.event;
-        switch (key.keyCode) {
-            case 38: // arrow up
-                moveX = player - 3;
-                moveY = player;
-                break;
-            case 40: // arrow down
-                moveX = player + 3;
-                moveY = player;
-                break;
-            case 39: // arrow right
-                moveX = player;
-                moveY = player + 3;
-                break;
-            case 37: // arrow left
-                moveX = player;
-                moveY = player - 3;
-                break;
-        }
-
-
-    }*/
-
 
 }); // end of jQuery
