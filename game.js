551a5a9985144f4d3ac2e360d789638b11ebd037
Daniel Raudschus <draudschus.mmt-b2013@fh.salzburg.ac.at>
2014-05-20 10:06:42 +0200

    minor changes

--- game.js
+++ game.js
@@ -68,7 +68,7 @@ var circles = new Array();
 
 
 function drawCircles() {
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < 50; i++) {
         var rad = Math.round(Math.random() * 200);       // from random rotation point! This is what should be checked about colliding with player
         var randomX = Math.round(Math.random() * (canvas.width + 200));
         var randomY = Math.round(Math.random() * (canvas.height + 200));
