f2cee87bb248e87050ccda8b9657025df1bdfea0
Daniel Raudschus <draudschus.mmt-b2013@fh.salzburg.ac.at>
2014-08-01 20:50:15 +0200

    grunt minifierf eingebaut

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,14 +16,24 @@ module.exports = function(grunt) {
                 ],
                 dest: 'js/release.js'
             }
+
+        },
+
+        uglify: {
+            build: {
+                src: 'js//release.js',
+                dest: 'js/release.min.js'
+            }
         }
 
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat']);
+    grunt.registerTask('default', ['concat', 'uglify']);
 
 };
