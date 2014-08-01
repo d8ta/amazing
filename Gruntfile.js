b49368c52b13240eddd2151538464afa59a2aee9
Daniel Raudschus <draudschus.mmt-b2013@fh.salzburg.ac.at>
2014-08-01 20:56:35 +0200

    image minifier eingebaut und imagesquelle im css umgestellt

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,6 +24,17 @@ module.exports = function(grunt) {
                 src: 'js//release.js',
                 dest: 'js/release.min.js'
             }
+        },
+
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'bilder/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'bilder/build/'
+                }]
+            }
         }
 
     });
@@ -31,9 +42,11 @@ module.exports = function(grunt) {
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+
 
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat', 'uglify']);
+    grunt.registerTask('default', ['concat', 'uglify', 'imagemin']);
 
 };
