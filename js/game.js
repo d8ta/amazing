12cfc624ab821a19a98c716800056be9f414fdab
Daniel Raudschus <draudschus.mmt-b2013@fh.salzburg.ac.at>
2014-08-09 10:08:28 +0200

    navleisten verschwinden, update

--- js/game.js
+++ js/game.js
@@ -12,10 +12,8 @@ context.canvas.width = window.innerWidth;
 // When ready...
 window.addEventListener("load",function() {
     // Set a timeout...
-    setTimeout(function(){
         // Hide the address bar!
         window.scrollTo(0, 1);
-    }, 0);
 });
 
 
