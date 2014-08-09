871256adfab4fde9d650ad30d5e1f97537a09470
Daniel Raudschus <draudschus.mmt-b2013@fh.salzburg.ac.at>
2014-08-09 09:52:51 +0200

    update setTimeout

--- js/game.js
+++ js/game.js
@@ -15,7 +15,7 @@ window.addEventListener("load",function() {
     setTimeout(function(){
         // Hide the address bar!
         window.scrollTo(0, 1);
-    }, 1);
+    }, 0);
 });
 
 
--- js/release.js
+++ js/release.js
@@ -24,7 +24,7 @@ window.addEventListener("load",function() {
     setTimeout(function(){
         // Hide the address bar!
         window.scrollTo(0, 1);
-    }, 1);
+    }, 0);
 });
 
 
